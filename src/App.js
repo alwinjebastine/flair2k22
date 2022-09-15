628cbaa16732cbacd8026392e3a588d7c29b3e9c
Crispin <crispinr003@gmail.com>
2022-09-15 11:50:18 +0530

    refactor: code cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BaseLayout>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/events" element={<BlogRoutes />} />
+            <Route path="/about" element={<Services />} />
+          </Routes>
+        </Router>
+      </BaseLayout>
+    </>
   )
 }
-
-export default App
