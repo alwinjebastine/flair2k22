f91516237bcb0961db910d63c3d59b2f4a3ee005
Daniel Thomas <danjes002@gmail.com>
2022-09-17 13:07:45 +0530

    refactor: minor change in App

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { BaseLayout } from './components/BaseLayout'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
-import { Events } from './pages/Events'
 import { Register } from './pages/Register'
 import './assets/scss/style.scss'
 
-export const App = () => {
+const App = () => {
   return (
     <BaseLayout>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
           <Route path="/about" element={<About />} />
           <Route path="/register" element={<Register />} />
         </Routes>
@@ -21,3 +19,5 @@ export const App = () => {
     </BaseLayout>
   )
 }
+
+export default App
