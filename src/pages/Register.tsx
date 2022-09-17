cfc6386ff2b2876f4a31db9004848856df08d79c
Crispin <crispinr003@gmail.com>
2022-09-18 02:41:11 +0530

    feat(register): upgrade UI/UX

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,6 +11,8 @@
 import { Checkbox, TextField, Autocomplete } from '@mui/material'
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
 import CheckBoxIcon from '@mui/icons-material/CheckBox'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
 const checkedIcon = <CheckBoxIcon fontSize="small" />
@@ -18,116 +20,120 @@ const checkedIcon = <CheckBoxIcon fontSize="small" />
 export const Register = () => {
   return (
     <>
-      <section className="d-flex justify-content-between bg-blue">
-        <div className="col-4 d-none d-md-block">
-          <img src="Bg.png" className="w-100" />
-        </div>
-        <div className="me-md-5 col-md-7 col-sm-12 text-center p-md-5">
-          <div className="m-5">
-            <div className="h3 mb-4">
+      <section className="container d-flex justify-content-center align-items-center w-100 my-5">
+        <Card
+          className="bg-dark all-text-white mt-5 rounded-curve shadow-white w-50 p-3 text-center"
+          style={{ minWidth: '18rem' }}
+        >
+          <CardContent>
+            <h4 className="h4 text-gradient mb-4">
               Take your technical journey with fun to a next step here! Register
               here and get your seats!
-            </div>
+            </h4>
             <hr />
-            <form className="p-md-3">
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">Register Number</label>
-                <input
-                  type="text"
-                  className="form-control bg-transparent"
-                  required
-                />
-              </div>
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">Name</label>
-                <input
-                  type="text"
-                  className="form-control bg-transparent"
-                  required
-                />
-              </div>
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">Email</label>
-                <input
-                  type="email"
-                  className="form-control bg-transparent"
-                  required
-                />
-              </div>
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">Phone Number</label>
-                <input
-                  type="tel"
-                  className="form-control bg-transparent"
-                  required
-                />
-              </div>
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">College Name</label>
-                <input
-                  type="text"
-                  className="form-control bg-transparent"
-                  required
-                />
-              </div>
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">Your Department</label>
-                <input
-                  type="text"
-                  className="form-control bg-transparent"
-                  required
-                />
-              </div>
-              <div className="my-4 text-start d-grid gap-2">
-                <label className="form-label">
-                  Select Your number of choices
-                </label>
-                <Autocomplete
-                  sx={{
-                    label: { color: 'white' },
-                    li: { color: 'white' },
-                    '& label.Mui-focused': {
-                      color: 'white',
-                    },
-                    '& .MuiOutlinedInput-root': {
-                      '& fieldset': {
-                        borderColor: 'white',
+            <form className="d-flex flex-column justify-content-center align-items-center">
+              <div className="w-75">
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">Register Number</label>
+                  <input
+                    type="text"
+                    className="form-control bg-transparent"
+                    required
+                  />
+                </div>
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">Name</label>
+                  <input
+                    type="text"
+                    className="form-control bg-transparent"
+                    required
+                  />
+                </div>
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    className="form-control bg-transparent"
+                    required
+                  />
+                </div>
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">Phone Number</label>
+                  <input
+                    type="tel"
+                    className="form-control bg-transparent"
+                    required
+                  />
+                </div>
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">College Name</label>
+                  <input
+                    type="text"
+                    className="form-control bg-transparent"
+                    required
+                  />
+                </div>
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">Your Department</label>
+                  <input
+                    type="text"
+                    className="form-control bg-transparent"
+                    required
+                  />
+                </div>
+                <div className="my-4 text-start d-grid gap-2">
+                  <label className="form-label">
+                    Select Your number of choices
+                  </label>
+                  <Autocomplete
+                    sx={{
+                      label: { color: 'white' },
+                      li: { color: 'white' },
+                      '& label.Mui-focused': {
+                        color: 'white',
                       },
-                      '&:hover fieldset': {
-                        borderColor: 'white',
+                      '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                          borderColor: 'white',
+                        },
+                        '&:hover fieldset': {
+                          borderColor: 'white',
+                        },
                       },
-                    },
-                  }}
-                  multiple
-                  id="checkboxes-tags-demo"
-                  options={events}
-                  disableCloseOnSelect
-                  className="all-text-white"
-                  getOptionLabel={(option) => option.title}
-                  renderOption={(props, option, { selected }) => (
-                    <li {...props}>
-                      <Checkbox
-                        icon={icon}
-                        checkedIcon={checkedIcon}
-                        style={{ marginRight: 8 }}
-                        checked={selected}
-                      />
-                      {option.title}
-                    </li>
-                  )}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Events" />
-                  )}
-                />
+                    }}
+                    multiple
+                    id="checkboxes-tags-demo"
+                    options={events}
+                    disableCloseOnSelect
+                    getOptionLabel={(option) => option.title}
+                    renderOption={(props, option, { selected }) => (
+                      <li {...props}>
+                        <Checkbox
+                          icon={icon}
+                          checkedIcon={checkedIcon}
+                          style={{ marginRight: 8 }}
+                          checked={selected}
+                        />
+                        {option.title}
+                      </li>
+                    )}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Events" />
+                    )}
+                  />
+                </div>
+                <button className="btn btn-gradient rounded-pill px-3 mt-2">
+                  Register!
+                </button>
               </div>
             </form>
-          </div>
-        </div>
+          </CardContent>
+        </Card>
       </section>
     </>
   )
 }
-const events = [
+export const events = [
   { title: 'Trade your theory' },
   { title: 'Glitch in the matrix' },
   { title: 'Merge Conflicts' },
